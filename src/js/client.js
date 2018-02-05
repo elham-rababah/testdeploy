1cabf47dbaa066d9472ef29f9eb368849d377d0b
Elham <elham.a.rababah@gmail.com>
2018-02-06 01:17:53 +0200

    first step from Routeing

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Router, Route, hashHistory,browserHistory} from 'react-router'
+
 import Layout from "./components/Layout";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render((
+	<Router history={browserHistory}>
+    <Route path="/" component={Layout}/>
+    {/* add the routes here */}
+    <Route path="/signin" component={Footer}/>
+    <Route path="/signup" component={Header}/>
+  </Router>
+), app);
--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router'
+
 
 export default class Header extends React.Component {
   constructor() {
@@ -13,6 +15,10 @@ export default class Header extends React.Component {
             <div class="navbar-header">
               <a class="navbar-brand" href="#"><img src="../assets/alist.png" width="45" height="35" class="d-inline-block align-top" alt="Awesome TodoList"/></a>
             </div>
+             <ul class="nav navbar-nav navbar-right">
+                <li><Link to="/signin" class="navbar-brand">Sign In</Link></li>
+                <li><Link to="/signup" class="navbar-brand">sign Up</Link></li>
+             </ul>
           </div>
         </nav>
       </div>
