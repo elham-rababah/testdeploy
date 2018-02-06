3ab5c424a8853b918e9e12d3744dd0d68644390a
Elhamrababah <Elham.a.rababah@gmail.com>
2018-02-06 15:05:22 +0200

    implement sign up in form and create state with some modify on indentation

--- src/js/components/SignUp/SignUp.js
+++ src/js/components/SignUp/SignUp.js
@@ -1,42 +1,117 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import { Link } from 'react-router'
+
 
 
 export default class SignIn extends React.Component {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+        this.state = {
+            name:'',
+            password:'',
+            password2:'',
+            email:'',
+        };
 
-  render() {
-    return (
-        <div>
-            <Header/>
-                <div class="container">
-                    <div class="col-md-6">
-                        <div id="logbox">
-                          <form id="signup" method="post" action="/signup">
-                            <h1>create an account</h1>
-                            <input name="user[name]" type="text" placeholder="What's your username?" pattern="^[\w]{3,16}$" autofocus="autofocus" required="required" class="input pass"/>
-                            <input name="user[password]" type="password" placeholder="Choose a password" required="required" class="input pass"/>
-                            <input name="user[password2]" type="password" placeholder="Confirm password" required="required" class="input pass"/>
-                            <input name="user[email]" type="email" placeholder="Email address" class="input pass"/>
-                            <input type="submit" value="Sign me up!" class="inputButton"/>
-                            <div class="text-center">
-                                already have an account? <a href="#" id="login_id">login</a>
+        this.addNewUser = this.addNewUser.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleChangePassword2 = this.handleChangePassword2.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+
+    }
+
+    addNewUser(event) {
+        console.log("addNewUser",event)
+        alert('Your favorite flavor is: ' +JSON.stringify(event) +JSON.stringify(this.state));
+    }
+    // TODO: its beter to handel all input filed togother in one function
+    handleChangeName(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleChangePassword2(event) {
+        this.setState({password2: event.target.value});
+    }
+
+    handleChangeEmail(event) {
+        this.setState({email: event.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                    <div class="container">
+                        <div class="col-md-6">
+                            <div id="logbox">
+                                <form id="signup" onSubmit={this.addNewUser}>
+                                    <h1>create an account</h1>
+                                    <input 
+                                        name="name"
+                                        type="text" 
+                                        placeholder="What's your Name?" 
+                                        pattern="^[\w]{3,16}$" 
+                                        autofocus="autofocus" 
+                                        required="required" 
+                                        class="input pass"
+                                        value={this.state.name}
+                                        onChange={this.handleChangeName}
+                                        />
+                                    <input 
+                                        name="password" 
+                                        type="password" 
+                                        placeholder="Choose a password" 
+                                        required="required" 
+                                        class="input pass" 
+                                        required="required"
+                                        value={this.state.password}
+                                        onChange={this.handleChangePassword}
+                                        />
+                                    <input 
+                                        name="password2" 
+                                        type="password" 
+                                        placeholder="Confirm password" 
+                                        required="required" 
+                                        class="input pass" 
+                                        value={this.state.password2}
+                                        onChange={this.handleChangePassword2}
+                                        />
+                                    <input 
+                                        name="email" 
+                                        type="email" 
+                                        placeholder="Email address" 
+                                        class="input pass" 
+                                        required="required"
+                                        value={this.state.email}
+                                        onChange={this.handleChangeEmail} 
+                                        />
+                                    <input 
+                                        type="submit" 
+                                        value="Sign me up!" 
+                                        class="inputButton"
+                                        />
+                                    <div class="text-center">
+                                        already have an account? <a id="login_id"><Link to="/signin">Sign In</Link></a>
+                                    </div>
+                                </form>
                             </div>
-                          </form>
                         </div>
-                    </div>
 
-                    <div class="col-md-6">
-                        <div id="logbox">
-                          Add some thing intrested in the future
+                        <div class="col-md-6">
+                            <div id="logbox">
+                                Add some thing intrested in the future
+                            </div>
                         </div>
                     </div>
-                </div>
-            <Footer/>
-        </div>
-    );
-  }
+                <Footer/>
+            </div>
+        );
+    }
 }
