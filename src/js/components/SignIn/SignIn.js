1eeadb7e291631613f2690a5d1f3981dcca959c0
Elham <elham.a.rababah@gmail.com>
2018-02-06 05:39:09 +0200

    add signin and signup template

--- src/js/components/SignIn/SignIn.js
+++ src/js/components/SignIn/SignIn.js
@@ -11,7 +11,26 @@ export default class SignIn extends React.Component {
     return (
         <div>
             <Header/>
-                signIn
+                <div class="container">
+                    <div class="col-md-6">
+                        <div id="logbox">
+                            <form id="signup" method="post" action="/signup">
+                                <h1>account login</h1>
+                                <input name="user[email]" type="email" placeholder="enter your email" class="input pass"/>
+                                <input name="user[password]" type="password" placeholder="enter your password" required="required" class="input pass"/>
+                                <input type="submit" value="Sign me in!" class="inputButton"/>
+                                <div class="text-center">
+                                    <a href="#" id="">create an account</a> - <a href="#" id="">forgot password</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div id="logbox">
+                          Add some thing intrested in the future
+                        </div>
+                    </div>
+                </div>
             <Footer/>
         </div>
     );
--- src/js/components/SignUp/SignUp.js
+++ src/js/components/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
+
 export default class SignIn extends React.Component {
   constructor() {
     super();
@@ -11,7 +12,29 @@ export default class SignIn extends React.Component {
     return (
         <div>
             <Header/>
-                SignUPPPPPPPPPP
+                <div class="container">
+                    <div class="col-md-6">
+                        <div id="logbox">
+                          <form id="signup" method="post" action="/signup">
+                            <h1>create an account</h1>
+                            <input name="user[name]" type="text" placeholder="What's your username?" pattern="^[\w]{3,16}$" autofocus="autofocus" required="required" class="input pass"/>
+                            <input name="user[password]" type="password" placeholder="Choose a password" required="required" class="input pass"/>
+                            <input name="user[password2]" type="password" placeholder="Confirm password" required="required" class="input pass"/>
+                            <input name="user[email]" type="email" placeholder="Email address" class="input pass"/>
+                            <input type="submit" value="Sign me up!" class="inputButton"/>
+                            <div class="text-center">
+                                already have an account? <a href="#" id="login_id">login</a>
+                            </div>
+                          </form>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div id="logbox">
+                          Add some thing intrested in the future
+                        </div>
+                    </div>
+                </div>
             <Footer/>
         </div>
     );
