2c87bb8bc2a9dd68bc8fa8fdd335194167b8f13a
Elham <elham.a.rababah@gmail.com>
2018-02-12 23:02:54 +0200

    fix the design alitle 2

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -13,7 +13,7 @@ export default class Header extends React.Component {
         <nav class="navbar navbar-default white">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#"><img src="../assets/alist.png" width="45" height="35" class="d-inline-block align-top" alt="Awesome TodoList"/></a>
+                    <a class="navbar-brand" href="#"><img src="../assets/alist.png" width="95" height="45" class="d-inline-block align-top" alt="Awesome TodoList"/></a>
                 </div>
                 <ul class="nav navbar-nav navbar-right">
                     <li><Link to="/signin" class="navbar-brand">Sign In</Link></li>
--- src/js/components/Header/HeaderWithLogin.js
+++ src/js/components/Header/HeaderWithLogin.js
@@ -19,7 +19,7 @@ export default class HeaderWithLogin extends React.Component {
             <nav class="navbar navbar-default white">
                 <div class="container-fluid">
                     <div class="navbar-header">
-                        <a class="navbar-brand" href="#"><img src="../assets/alist.png" width="45" height="35" class="d-inline-block align-top" alt="Awesome TodoList"/></a>
+                        <a class="navbar-brand" href="#"><img src="../assets/alist.png" width="95" height="45" class="d-inline-block align-top" alt="Awesome TodoList"/></a>
                     </div>
                         <ul class="nav navbar-nav navbar-right">
                             <li><Link to="#/" onClick={this.logout} class="navbar-brand">logout</Link></li>
